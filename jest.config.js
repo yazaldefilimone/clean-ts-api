1663da8fa7673d943c9d926fd96102cf4ca6b25b
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-12 17:45:08 +0200

    chore: remove config

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,12 @@
 module.exports = {
-  roots: ["<rootDir>/tests"],
+  roots: ["<rootDir>/src"],
   collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/main/**"],
   coverageDirectory: "coverage",
-  coverageProvider: "babel",
   testEnvironment: "node",
-  preset: "@shelf/jest-mongodb",
   transform: {
     ".+\\.ts$": "ts-jest",
   },
   moduleNameMapper: {
-    "@/tests/(.*)": "<rootDir>/tests/$1",
     "@/(.*)": "<rootDir>/src/$1",
   },
 };
